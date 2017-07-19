33dbb36fe40cbf2e5faad9489da0aec9a90496d0
sjaspeer <snapalit_1400330@uic.edu.ph>
2017-07-19 12:13:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
-app.get('/index', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
-});
 app.get('/Assignment1', function(req, res){
   res.sendFile(path.join(__dirname, 'views/Assignment1.html'));
 });
@@ -19,6 +16,9 @@ app.get('/assignment2', function(req, res){
 });
 app.get('/Assignment3', function(req, res){
  res.sendFile(path.join(__dirname, 'views/Assignment3.html'));
+});
+app.get('/index', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 //express server listen
 var server = app.listen(app.get('port'), function(){
