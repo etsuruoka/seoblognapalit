fbf46fe82bb64491f4eb6357bc6fc786628deeb9
etsuruoka <etsuruoka_1400228@uic.edu.ph>
2017-07-21 08:32:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//require modules
 var express = require('express');
 var path = require('path');
 //instantiate express
@@ -8,17 +7,20 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 app.get('/Assignment1', function(req, res){
   res.sendFile(path.join(__dirname, 'views/Assignment1.html'));
 });
-app.get('/assignment2', function(req, res){
+app.get('/Assignment2', function(req, res){
   res.sendFile(path.join(__dirname, 'views/Assignment2.html'));
 });
 app.get('/Assignment3', function(req, res){
- res.sendFile(path.join(__dirname, 'views/Assignment3.html'));
+  res.sendFile(path.join(__dirname, 'views/Assignment3.html'));
 });
-app.get('/index', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
 //express server listen
 var server = app.listen(app.get('port'), function(){
